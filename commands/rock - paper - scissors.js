becb98cf3f93b482c6e240ed1dc6e933472f4116
stamsarg <stamatios.sargantanis@gmail.com>
2016-01-05 16:58:42 +0200

    indentation problems

--- commands/rock - paper - scissors.js
+++ commands/rock - paper - scissors.js
@@ -1,15 +1,15 @@
 function Game(session, bot, channel, cmd, args, nick, text, time, isadmin, trip)
 {
-	var rps = {
+    var rps = {
         rock: ["Rock. It's a tie!", "Scissors. Rock wins!", "Paper. Paper wins!"],
         paper: ["Rock. Paper wins!", "Scissors. Scissors wins!", "Paper. It's a tie!"],
         scissors: ["Rock. Rock wins!", "Scissors. It's a tie!", "Paper. Scissors wins!"]
-	};
-  bot.choose(rps[cmd]);
+    };
+    bot.choose(rps[cmd]);
 }
 
 module.exports = {
-	rock: Game,
-	paper: Game,
-	scissors: Game
+    rock: Game,
+    paper: Game,
+    scissors: Game
 };
