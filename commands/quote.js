e1907aa4427fd489f580d5adf42e7cbc21111322
stamsarg <stamatios.sargantanis@gmail.com>
2016-01-05 17:05:02 +0200

    Create quote.js

---
+++ commands/quote.js
@@ -0,0 +1,8 @@
+function GenerateRandomQuote(session, bot, channel, cmd, args, nick, text, time, isadmin, trip)
+{
+	bot.choose(bot.messages);
+}
+
+module.exports = {
+	quote: GenerateRandomQuote
+};
